cc1d973dd4f5381f50ae449795240c952a58876c
Romerrro2701 <Romerrro2701@gmail.com>
2026-09-14 19:38:44 -0300

    Add multilingual catalogue test
    Added a smoke test to verify the presence of the 'Add to basket' button on the product page.

---
+++ test_items.py
@@ -0,0 +1,20 @@
+"""Locale-aware smoke test for the Stepik Selenium course."""
+
+from selenium.webdriver.common.by import By
+
+
+CATALOGUE_URL = (
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+)
+
+
+def test_guest_can_see_add_to_basket_button(browser):
+    browser.get(CATALOGUE_URL)
+
+    add_to_basket_buttons = browser.find_elements(
+        By.CSS_SELECTOR, ".btn-add-to-basket"
+    )
+
+    assert len(add_to_basket_buttons) == 1, (
+        "The page must contain exactly one Add to basket button"
+    )
