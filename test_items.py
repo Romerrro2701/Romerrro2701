647537bb79095f4d93dacb79c697df5610c87f92
Romerrro2701 <Romerrro2701@gmail.com>
2026-09-14 19:47:26 -0300

    Wait for localized product page
    Added a sleep statement to wait before checking buttons.

--- test_items.py
+++ test_items.py
@@ -1,5 +1,7 @@
 """Locale-aware smoke test for the Stepik Selenium course."""
 
+from time import sleep
+
 from selenium.webdriver.common.by import By
 
 
@@ -10,6 +12,7 @@ CATALOGUE_URL = (
 
 def test_guest_can_see_add_to_basket_button(browser):
     browser.get(CATALOGUE_URL)
+    sleep(30)
 
     add_to_basket_buttons = browser.find_elements(
         By.CSS_SELECTOR, ".btn-add-to-basket"
